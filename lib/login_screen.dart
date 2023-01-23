89158e246e034080b728b34a0cb4922cdfc0e194
shubham upadhyay <shubhamupadhyay@shubhams-MacBook-Air.local>
2023-01-23 20:12:41 +0530

    Login UI done, functionalities remaining

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -44,12 +44,103 @@ class TabletLogin extends StatelessWidget {
 class DesktopLogin extends StatelessWidget {
   const DesktopLogin({Key? key}) : super(key: key);
 
+
+  Widget loginWithGoogle(){
+    return Container(
+      decoration: const BoxDecoration(
+          color: Colors.white
+      ),
+
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Icon(FontAwesomeIcons.google),
+              SizedBox(width: 20,),
+              Text("Sign In with Google"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget loginWithHiveSigner(){
+    return Container(
+      decoration: const BoxDecoration(
+          color: Color(0xffe31337)
+      ),
+
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('assets/hivesigner.png', gaplessPlayback: true, width: 18,),
+              const SizedBox(width: 20,),
+              const Text("Login with HiveSigner", style: TextStyle(color: Colors.white),),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget loginWithUnstoppable(){
+    return Container(
+      decoration: const BoxDecoration(
+          color: Color(0xff0D66FE)
+      ),
+
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('assets/unstoppable.png', gaplessPlayback: true, width: 25,),
+              const SizedBox(width: 20,),
+              const Text("Login with Unstoppable", style: TextStyle(color: Colors.white),),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Expanded(flex: 1,child: Container(color: Colors.blue, )),
-        Expanded(flex: 1,child: Container(color: Colors.black,)),
+        Expanded(flex: 3,child: Container(color: Colors.blue, )),
+        Expanded(flex: 2,child: Container(color: Color(0xff191919),
+        child: Row(
+          children: [
+            Expanded(flex: 1,child: Container()),
+            Expanded(flex: 2,child: Container(
+
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  loginWithGoogle(),
+                  SizedBox(height: 10,),
+                  loginWithHiveSigner(),
+                  SizedBox(height: 10,),
+                  loginWithUnstoppable()
+
+                ],
+              ),
+            )),
+            Expanded(flex: 1,child: Container()),
+          ],
+        ),
+        )),
       ],
     );
   }
