453bd9d7fe1ce27dbdf6f26873191d889d3d2df3
shubham upadhyay <shubhamupadhyay@shubhams-MacBook-Air.local>
2023-01-25 20:22:04 +0530

    Hivesigner initiated

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -2,12 +2,13 @@
 
  import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:dio/dio.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -80,52 +81,54 @@ class DesktopLogin extends StatelessWidget {
   Widget loginWithHiveSigner(BuildContext context){
     return GestureDetector(
       onTap: ()async{
-        await showDialog(context: context, builder: (context){
-          // return Dialog(
-          //   child: WebView(
-          //     // gestureRecognizers: Set()
-          //     //   ..add(
-          //     //     Factory<VerticalDragGestureRecognizer>(
-          //     //           () => VerticalDragGestureRecognizer(),
-          //     //     ), // or null
-          //     //   ),
-          //     javascriptMode: JavascriptMode.unrestricted,
-          //     initialUrl:
-          //     'https://hivesigner.com/oauth2/authorize?client_id=aureal&redirect_uri=%3Dhttp%253A%252F%252Flocalhost%253A3000%26&response_type=code&scope=offline,comment,vote,comment_option,custom_json',
-          //     onPageStarted: (url) async {
-          //       SharedPreferences prefs =
-          //       await SharedPreferences.getInstance();
-          //
-          //       var uri = Uri.parse(url.toString());
-          //       uri.queryParameters.forEach((key, value) {
-          //         if (key == 'access_token' ||
-          //             key == 'username' ||
-          //             key == 'code') {
-          //           if (key == 'username' &&
-          //               prefs.getString('access_token') != null &&
-          //               prefs.getString('code') != null) {
-          //             prefs.setString('HiveUserName', value);
-          //
-          //             print(prefs.getString('code'));
-          //
-          //
-          //           } else {
-          //             prefs.setString('$key', value);
-          //           }
-          //
-          //           print(prefs.getString(key));
-          //           if (prefs.getString('code') != null) {
-          //             print(
-          //                 'this is the code motherfucker: ${prefs.getString('code')}');
-          //           }
-          //           // hiveAuth();
-          //         }
-          //       });
-          //     },
-          //   ),
-          // );
-          return Container();
-        });
+        // await showDialog(context: context, builder: (context){
+        //   // return Dialog(
+        //   //   child: WebView(
+        //   //     // gestureRecognizers: Set()
+        //   //     //   ..add(
+        //   //     //     Factory<VerticalDragGestureRecognizer>(
+        //   //     //           () => VerticalDragGestureRecognizer(),
+        //   //     //     ), // or null
+        //   //     //   ),
+        //   //     javascriptMode: JavascriptMode.unrestricted,
+        //   //     initialUrl:
+        //   //     'https://hivesigner.com/oauth2/authorize?client_id=aureal&redirect_uri=%3Dhttp%253A%252F%252Flocalhost%253A3000%26&response_type=code&scope=offline,comment,vote,comment_option,custom_json',
+        //   //     onPageStarted: (url) async {
+        //   //       SharedPreferences prefs =
+        //   //       await SharedPreferences.getInstance();
+        //   //
+        //   //       var uri = Uri.parse(url.toString());
+        //   //       uri.queryParameters.forEach((key, value) {
+        //   //         if (key == 'access_token' ||
+        //   //             key == 'username' ||
+        //   //             key == 'code') {
+        //   //           if (key == 'username' &&
+        //   //               prefs.getString('access_token') != null &&
+        //   //               prefs.getString('code') != null) {
+        //   //             prefs.setString('HiveUserName', value);
+        //   //
+        //   //             print(prefs.getString('code'));
+        //   //
+        //   //
+        //   //           } else {
+        //   //             prefs.setString('$key', value);
+        //   //           }
+        //   //
+        //   //           print(prefs.getString(key));
+        //   //           if (prefs.getString('code') != null) {
+        //   //             print(
+        //   //                 'this is the code motherfucker: ${prefs.getString('code')}');
+        //   //           }
+        //   //           // hiveAuth();
+        //   //         }
+        //   //       });
+        //   //     },
+        //   //   ),
+        //   // );
+        //   return Container();
+        // });
+        final result = await FlutterWebAuth.authenticate(url: "https://hivesigner.com/oauth2/authorize?client_id=aureal&redirect_uri=%3Dhttp%253A%252F%252Flocalhost%253A3000%26&response_type=code&scope=offline,comment,vote,comment_option,custom_json", callbackUrlScheme: "hivesigner");
+        print(result);
       },
       child: Container(
         decoration: const BoxDecoration(
